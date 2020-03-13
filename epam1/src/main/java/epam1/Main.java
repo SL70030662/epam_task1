bcc5ed1ffabe919bbf739346f67d52a6d654a988
Akhil <Akhil@LAPTOP-3O8A24HK>
2020-03-13 22:21:42 +0530

    ok

---
+++ epam1/src/main/java/epam1/Main.java
@@ -0,0 +1,18 @@
+package epam1;
+
+
+public class Main {
+	public static void main(String []args)
+	{
+	int same;
+	child c=new child();
+	same=c.same;
+	System.out.println(same);
+	}
+}
+	 class child{
+	int same=989;
+	}
+
+
+
---
+++ epam1/src/main/java/epam1/hack.java
@@ -0,0 +1,31 @@
+package epam1;
+
+import java.util.Collections;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class hack {
+	 public static void main(String args[])
+	    {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt(),i;
+		String[] s=new String[n];
+		for(i=0;i<n;i++)
+		{
+			s[i]=sc.next();
+		}
+		Map<String, Integer> map = new TreeMap<>(Collections.reverseOrder());
+	    for (String vote : s) {
+	        if (map.containsKey(vote)) {
+	            map.put(vote, map.get(vote) + 1);
+	        } else {
+	            map.put(vote, 1);
+	        }
+	    }
+	    System.out.println(Collections.max(map.entrySet(), Map.Entry.comparingByValue())
+	            .getKey());
+		sc.close();
+	}
+
+}
